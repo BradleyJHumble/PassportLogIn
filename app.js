899dec7d5a0d813933dd147659e61322b5c18bcd
judikial <bradleyjhumble@gmail.com>
2017-11-13 02:05:04 -0600

    Server Runs!
    .

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var passport = require('passport');
+var expressValidator = require('express-validator');
 var LocalStratergy = require('passport-local').Stratergy;
 var multer = require('multer');
+var upload = multer({dest: './uploads'});
 var flash = require('connect-flash');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 var index = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -29,9 +30,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Handle File uploads using Multer
-app.use(multer(dest:./uplads));
 
+// Handles Sessions
+app.use(session({
+	secret:'secret',
+	saveUnititialized: true,
+	resave: true
+}));
+
+// Handles Passport Middlewear
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Handles Validator
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value){
+		var namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+
+		while(namespace.length){
+			formParam+= '[' + namespace.shift() + ']';
+		} return {
+			param: formParam,
+			msg : msg,
+			value : value
+		};
+	}
+}));
+
+// Handles Express-Msg
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 // Routes Variables!
 app.use('/', index);
